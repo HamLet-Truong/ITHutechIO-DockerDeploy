05d480c69d2639931023d868ae43e4b43e3ed5e5
HamLet-Truong <hamlet1802@gmail.com>
2026-03-26 11:14:18 +0700

    Init prroject

---
+++ src/main/java/com/example/DockerProfile/DockerProfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DockerProfile/DockerProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
